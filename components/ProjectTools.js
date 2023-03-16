1c3f39b99104a519979c56fb3b7e057dddd12a42
Eve Cho <e.young.echo@gmail.com>
2023-03-16 16:57:34 -0400

    feature: ProjectTools component created

---
+++ components/ProjectTools.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { ReactDOM } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from '../styles/Home.module.css'
+
+export default function ProjectTools(props) {
+    const tools = props.tools;  // tools will be an array of the tech stack used for project
+    const toolsFormatted = tools.join(' / ');
+
+    return (
+        <div className={''}>
+            <p>Created with: {tools}</p>
+        </div>
+    )
+}
