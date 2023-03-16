9d4414cf2246b17cec1e96a92774945bc2ad21e3
Eve Cho <e.young.echo@gmail.com>
2023-03-16 17:00:20 -0400

    feature: ProjectDescription component created

---
+++ components/ProjectDescription.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import { ReactDOM } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from '../styles/Home.module.css'
+
+export default function ProjectDescription(props) {
+    const description = props.description;
+
+    return (
+        <div className={''}>
+            <p>{description}</p>
+        </div>
+    )
+}
