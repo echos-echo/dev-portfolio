5ee0ca904705bb94ad3324f5b58d5b0ad4dda777
Eve Cho <e.young.echo@gmail.com>
2023-03-16 16:54:39 -0400

    feature: ProjectLinks.js where links relevant to a project will go created

---
+++ components/ProjectLinks.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { ReactDOM } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from '../styles/Home.module.css'
+
+export default function ProjectLinks(props) {
+    const repo = props.repo;
+    const siteURL = props.siteURL || null;
+    const projectName = props.name;
+
+    return (
+        <div className={''}>
+            placeholder but this will be where links relevant to a project will go
+            <br/>
+            {/* repository link */}
+            <a href={repo}>Check out {projectName}&apos;s repository</a>
+            {/* site link ONLY if it exists */}
+            {siteURL !== null ? <a href={repo}>Check out {projectName} here</a> : null}
+        </div>
+    )
+}
