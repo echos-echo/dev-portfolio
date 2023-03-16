5d2633ce26e9fe2b753486cc01631b340a9a90bd
Eve Cho <e.young.echo@gmail.com>
2023-03-16 17:03:22 -0400

    feature: ProjectImage component created

---
+++ components/ProjectImage.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import { ReactDOM } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from '../styles/Home.module.css'
+
+export default function ProjectImage(props) {
+    const imagePath = props.imagePath;  // tools will be an array of the tech stack used for project
+    const height = props.height;
+    const width = props.width;
+    const alt = props.alt;
+
+    return (
+        <div className={''}>
+            <Image src={imagePath} height={height} width={width} alt={alt} quality={90}/>
+        </div>
+    )
+}
