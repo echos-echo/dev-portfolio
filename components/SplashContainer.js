73e47a7220235b99bf628ffa2c902e0828ab876c
Eve Cho <e.young.echo@gmail.com>
2023-03-27 16:14:27 -0400

    styling: adjusted font and margins on pages

--- components/SplashContainer.js
+++ components/SplashContainer.js
@@ -5,10 +5,11 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import SplashTitle from "./SplashTitle";
 import SplashDescription from "./SplashDescription";
+import styles from "../styles/Home.module.css";
 
 export default function SplashContainer() {
     return (
-        <div>
+        <div className={styles.splashContainer}>
             <SplashTitle/>
             <SplashDescription/>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main className={styles.splash}>
         <SplashContainer/>
           <h1>SITE UNDER CONSTRUCTION</h1>
       </main>
