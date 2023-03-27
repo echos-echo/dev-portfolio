2a3dc92933775757c7beb7e7b00643e80b2f74b4
Eve Cho <e.young.echo@gmail.com>
2023-03-27 17:28:28 -0400

    styling: replaced arrow in menu with mui icon

--- components/NavButtons.js
+++ components/NavButtons.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from '../styles/Home.module.css'
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
 
 export default function NavButtons(props) {
     const link = '/' + props.href;
@@ -13,7 +14,7 @@ export default function NavButtons(props) {
     return (
         <Link href={link} className={styles.navButton}>
             <div >
-                {buttonLabel}{pathname.includes(link) ? ' ⟽' : null}
+                {buttonLabel}{pathname.includes(link) ? <KeyboardDoubleArrowLeftIcon/> : null}
             </div>
         </Link>
     )
