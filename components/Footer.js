7a8e2f90e8300e9d44520882d5caff373f9a15d9
Eve Cho <e.young.echo@gmail.com>
2023-03-27 19:00:14 -0400

    feature: added github link for the portfolio to the footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,12 +4,15 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from '../styles/Home.module.css'
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Footer() {
 
     return (
         <div className={styles.footer}>
-            placeholder but this will be a footer
+            <a href='https://github.com/echos-echo/dev-portfolio' target={'_blank'} rel={'noreferrer'}>
+                <GitHubIcon sx={{fontSize: 40}}/>
+            </a>
         </div>
     )
 }
